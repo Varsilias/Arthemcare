0e41f43c2446ea61e231d60dac13c94eca39bcac
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2021-12-19 19:13:41 +0100

    added live URL

--- src/http/api.js
+++ src/http/api.js
@@ -3,8 +3,8 @@ import * as axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
-    // baseURL: 'https://arthemcare-api.herokuapp.com/api/v1',
+    // baseURL: 'http://localhost:8000/api/v1',
+    baseURL: 'https://arthemcare-api.herokuapp.com/api/v1',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
